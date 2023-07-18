e85d13dc8773904b48025c52610bc9d24fa7b229
Recep ÇAVUŞOĞLU <altmisbir@msn.com>
2023-07-18 17:25:52 +0300

    day5 Task1

--- src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radio/T6_StaleElementReferenceException.java
+++ src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radio/T6_StaleElementReferenceException.java
@@ -23,14 +23,17 @@ public class T6_StaleElementReferenceException {
 
         //3- Locate “CYDEO” link, verify it is displayed.
         WebElement cydeoLink = driver.findElement(By.xpath("//a[text()='CYDEO']"));
+
         System.out.println("cydeoLink.isDisplayed() = " + cydeoLink.isDisplayed());
 
         //4- Refresh the page.
         driver.navigate().refresh();
+
+        //We are refreshing the web element reference by re-assigning (re-locating) the web element
+        cydeoLink = driver.findElement(By.xpath("//a[text()='CYDEO']"));
+
         //5- Verify it is displayed, again.
         System.out.println("cydeoLink.isDisplayed() = " + cydeoLink.isDisplayed());
-
-
 
     }
 
