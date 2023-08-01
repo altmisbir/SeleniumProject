320fb74c725c57cc976d887b5478f903ace1353c
Recep ÇAVUŞOĞLU <altmisbir@msn.com>
2023-08-01 22:18:26 +0300

    day9 JavaFakerPractice.java

--- src/test/java/com/cydeo/tests/day9_javafaker_testbase_driverUtil/JavaFakerPractice.java
+++ src/test/java/com/cydeo/tests/day9_javafaker_testbase_driverUtil/JavaFakerPractice.java
@@ -3,12 +3,14 @@ package com.cydeo.tests.day9_javafaker_testbase_driverUtil;
 import com.github.javafaker.Faker;
 import org.testng.annotations.Test;
 
+import java.util.Locale;
+
 public class JavaFakerPractice {
 
     @Test
     public void test1() {
         //Creating Faker onject to reach methods
-        Faker faker = new Faker();
+        Faker faker = new Faker(new Locale("tr"));
 
         System.out.println("faker.name().firstName() = " + faker.name().firstName());
         System.out.println("faker.name().lastName() = " + faker.name().lastName());
