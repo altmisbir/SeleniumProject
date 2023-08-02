1d79170c11894be3a18e2133cfa7762bb0927045
Recep ÇAVUŞOĞLU <altmisbir@msn.com>
2023-08-02 23:03:25 +0300

    day10 T1_Registration_Form.java

--- src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T1_Registration_Form.java
+++ src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T1_Registration_Form.java
@@ -1,5 +1,10 @@
 package com.cydeo.tests.day10_upload_actions_jsexecutor;
 
+import com.cydeo.utilities.ConfigurationReader;
+import com.cydeo.utilities.Driver;
+import com.github.javafaker.Faker;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class T1_Registration_Form {
@@ -7,10 +12,29 @@ public class T1_Registration_Form {
     public void registration_from_test() {
         //1. Open browser
         //2. Go to website: https://practice.cydeo.com/registration_form
+        Driver.getDriver().get(ConfigurationReader.getProperty("registration.form.url"));
+
+        //Create JavaFaker object
+        Faker faker = new Faker();
+
         //3. Enter first name
+        WebElement inputFirstName = Driver.getDriver().findElement(By.xpath("//input[@name='firstname']"));
+        inputFirstName.sendKeys(faker.name().firstName());
+
         //4. Enter last name
+        WebElement inputLastName = Driver.getDriver().findElement(By.xpath("//input[@name='lastname']"));
+        inputLastName.sendKeys(faker.name().lastName());
+
         //5. Enter username
+        WebElement inputUserName = Driver.getDriver().findElement(By.xpath("//input[@name='username']"));
+        //inputUserName.sendKeys(faker.name().username().replaceAll(".",""));
+        String user = faker.bothify(faker.bothify("???###"));
+        inputUserName.sendKeys(user);
+
         //6. Enter email address
+        WebElement inputEmail = Driver.getDriver().findElement(By.xpath("//input[@name='email']"));
+        inputEmail.sendKeys(user + "@email.com");
+
         //7. Enter password
         //8. Enter phone number
         //9. Select a gender from radio buttons
